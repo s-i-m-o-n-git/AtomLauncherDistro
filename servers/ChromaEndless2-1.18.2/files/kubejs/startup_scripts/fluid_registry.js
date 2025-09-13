efb0e542d6090c1ed20e43215a4849799544325d
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:52 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/startup_scripts/fluid_registry.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/startup_scripts/fluid_registry.js
@@ -0,0 +1,26 @@
+onEvent('fluid.registry', (event) => {
+    const fluids = [
+      //Molten Mana Fluid
+      {
+        type: 'thick',
+        id: 'molten_mana',
+        texture: 0x85c0e6,
+        display: '§3Molten Mana'
+      },
+      //Molten Insanium
+      {
+        type: 'thick',
+        id: 'molten_insanium',
+        texture: 0x940cef,
+        display: 'Molten Insanium'
+      },
+    ];
+
+    fluids.forEach((fluid) => {
+        if (fluid.type == 'thick') {
+            event.create('chroma:' + fluid.id).thickTexture(fluid.texture).bucketColor(fluid.texture).displayName(fluid.display);
+        } else if (fluid.type == 'thin') {
+            event.create('chroma:' + fluid.id).thinTexture(fluid.texture).bucketColor(fluid.texture).displayName(fluid.display);
+        }
+    });
+});

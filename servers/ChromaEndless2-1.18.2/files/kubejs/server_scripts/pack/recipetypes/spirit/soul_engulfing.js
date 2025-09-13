d9d9e91364925798d011520158f3107edb2a9983
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:46 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/spirit/soul_engulfing.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/spirit/soul_engulfing.js
@@ -0,0 +1,52 @@
+onEvent("recipes", (event) => {
+    const id_prefix = 'chroma:pack/spirit/soul_engulfing/';
+    const recipes = [
+      {
+          ingredient: { item: 'minecraft:blackstone' },
+          block: 'blue_skies:mossy_lunar_stonebrick',
+          duration: 5,
+          destroysStructure: false,
+          outputItem: 'blue_skies:lunar_stone',
+          id: `${id_prefix}lunar_stone`
+      },
+      {
+        ingredient: { item: 'minecraft:cauldron' },
+        block: 'botania:livingrock',
+        duration: 5,
+        destroysStructure: false,
+        outputItem: 'botania:diluted_pool',
+        id: `${id_prefix}botania:diluted_pool`
+    },
+    ];
+
+    recipes.forEach((recipe) => {
+      event.custom({
+        type: "spirit:soul_engulfing",
+        input: {
+          ingredient: recipe.ingredient,
+          multiblock: {
+            pattern: [
+              [
+                "   ",
+                " @ ",
+                "   "
+              ],
+              [
+                " S ",
+                "S&S",
+                " S "
+              ]
+            ],
+            keys: {
+              S: {
+                block: recipe.block
+              }
+            }
+          }
+        },
+        duration: recipe.duration,
+        destroysStructure: recipe.destroysStructure,
+        outputItem: recipe.outputItem
+      }).id(recipe.id);
+    });
+});

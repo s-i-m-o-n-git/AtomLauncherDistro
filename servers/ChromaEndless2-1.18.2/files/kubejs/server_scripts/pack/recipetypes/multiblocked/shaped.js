8dfcd4df223f7d5a5c9a8947eb83c90217c6682c
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:40 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/multiblocked/shaped.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/multiblocked/shaped.js
@@ -0,0 +1,81 @@
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/multiblocked/shaped/';
+    const recipes = [
+        {
+            output: 'chroma:brass_create_input',
+            pattern: ['BBB', 'MGB', 'BBB'],
+            key: {
+                B: 'create:brass_casing',
+                M: 'create:rotation_speed_controller',
+                G: 'create:large_cogwheel'
+            },
+            id: `${id_prefix}brass_create_input`
+        },
+        {
+            output: 'chroma:brass_create_output',
+            pattern: ['BBB', 'BMG', 'BBB'],
+            key: {
+                B: 'create:brass_casing',
+                M: 'create:rotation_speed_controller',
+                G: 'create:large_cogwheel'
+            },
+            id: `${id_prefix}brass_create_output`
+        },
+        {
+            output: 'chroma:create_item_output',
+            pattern: ['BBB', 'MGB', 'BBB'],
+            key: {
+                B: 'create:brass_casing',
+                M: 'create:item_vault',
+                G: 'create:precision_mechanism'
+            },
+            id: `${id_prefix}create_item_output`
+        },
+        {
+            output: 'chroma:create_item_input',
+            pattern: ['BBB', 'BMG', 'BBB'],
+            key: {
+                B: 'create:brass_casing',
+                M: 'create:item_vault',
+                G: 'create:precision_mechanism'
+            },
+            id: `${id_prefix}create_item_input`
+        },
+        {
+            output: 'chroma:energy_coke_input',
+            pattern: ['CHC', 'HAH', 'CHC'],
+            key: {
+                C: 'immersiveengineering:coil_hv',
+                H: '#forge:ingots/hop_graphite',
+                A: 'immersiveengineering:capacitor_hv'
+            },
+            id: `${id_prefix}energy_coke_input`
+        },
+        {
+            output: 'chroma:coke_item_input',
+            pattern: ['CHC', 'SAS', 'CHC'],
+            key: {
+                C: 'immersiveengineering:radiator',
+                H: '#forge:ingots/steel',
+                A: 'immersiveengineering:crate',
+                S: 'immersiveengineering:component_steel'
+            },
+            id: `${id_prefix}coke_item_input`
+        },
+        {
+            output: 'chroma:coke_item_output',
+            pattern: ['CSC', 'HAH', 'CSC'],
+            key: {
+                C: 'immersiveengineering:radiator',
+                H: '#forge:ingots/steel',
+                A: 'immersiveengineering:crate',
+                S: 'immersiveengineering:component_steel'
+            },
+            id: `${id_prefix}coke_item_output`
+        },
+    ];
+
+    recipes.forEach((recipe) => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+    });
+});

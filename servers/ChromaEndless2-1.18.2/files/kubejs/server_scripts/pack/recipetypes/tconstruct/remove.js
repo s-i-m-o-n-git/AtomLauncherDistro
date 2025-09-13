04cf73390630bcfebb2263f2c92661d1cbf4980a
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:47 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/tconstruct/remove.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/tconstruct/remove.js
@@ -0,0 +1,28 @@
+// priority: 100
+
+onEvent('recipes', (event) => {
+    const idRemovals = [
+      'tconstruct:tables/tinkers_anvil',
+      'tconstruct:tables/scorched_anvil',
+      'tconstruct:smeltery/seared/drain_retextured',
+      'tconstruct:smeltery/seared/duct_retextured',
+      'tconstruct:smeltery/seared/chute_retextured',
+      'tconstruct:smeltery/scorched/duct_retextured',
+      'tconstruct:smeltery/scorched/drain_retextured',
+      'tconstruct:smeltery/scorched/chute_retextured',
+      'tconstruct:smeltery/alloys/molten_brass',
+      'tconstruct:common/flint'
+    ];
+
+    const outputRemovals = [
+
+    ];
+
+    idRemovals.forEach((id) => {
+        event.remove({ id: id });
+    });
+
+    outputRemovals.forEach((output) => {
+        event.remove({ output: output });
+    });
+});

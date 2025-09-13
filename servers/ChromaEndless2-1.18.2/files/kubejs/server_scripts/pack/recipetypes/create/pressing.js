c01eabd26ae5135f0481770643ca6a894827f826
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:23 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/create/pressing.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/create/pressing.js
@@ -0,0 +1,30 @@
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/create/pressing/';
+
+    const recipes = [
+      {
+          output: 'chroma:mana_diamond_plate',
+          inputs: ['botania:mana_diamond'],
+          id: `${id_prefix}mana_diamond_plate`
+      },
+      {
+          output: 'create:sand_paper',
+          inputs: ['minecraft:sand'],
+          id: 'create:crafting/materials/sand_paper'
+      },
+      {
+          output: 'create:red_sand_paper',
+          inputs: ['minecraft:red_sand'],
+          id: 'create:crafting/materials/red_sand_paper'
+      },
+      {
+          output: 'chroma:resin_plate',
+          inputs: ['chroma:raw_resin', 'mob_grinding_utils:solid_xp_mould_blank'],
+          id: `${id_prefix}resin_plate`
+      },
+    ];
+
+    recipes.forEach((recipe) => {
+        event.recipes.create.pressing(recipe.output, recipe.inputs).id(recipe.id);
+    });
+});

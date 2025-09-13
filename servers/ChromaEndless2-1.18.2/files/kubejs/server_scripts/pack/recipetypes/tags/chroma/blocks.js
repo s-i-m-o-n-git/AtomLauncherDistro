babfc8a1d7f89091a643a04cb8228808259864bc
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:46 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/tags/chroma/blocks.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/tags/chroma/blocks.js
@@ -0,0 +1,8 @@
+onEvent('block.tags', (event) => {
+    event.add('forge:storage_blocks/dragonstone', 'botania:dragonstone_block');
+    event.add('forge:storage_blocks/mana_diamond', 'botania:mana_diamond_block');
+    event.add('minecraft:beacon_base_blocks', 'chroma:void_miner_frame_tier1');
+    event.add('minecraft:beacon_base_blocks', 'chroma:void_miner_frame_tier2');
+    event.add('minecraft:beacon_base_blocks', 'chroma:void_miner_frame_tier3');
+    event.add('forge:ores/crystaltine', 'chroma:glacio_crystaltine_ore');
+});

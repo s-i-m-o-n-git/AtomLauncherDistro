28bce30ac87ffd28b5e3fe0e9fb7530fab1687b4
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:38 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/mekanism/remove.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/mekanism/remove.js
@@ -0,0 +1,24 @@
+onEvent('recipes', (event) => {
+    const idRemovals = [
+        'mekanism:structural_glass',
+        'morevanillalib:obsidian_shard_decompress',
+        'jaopca:immersiveengineering.material_to_dust.obsidian',
+        'lazierae2:compat/mekanism/grinder/dust_obsidian',
+        'jaopca:mekanism.material_to_dust.obsidian',
+        'mekanism:enriching/conversion/obsidian_to_obsidian_dust',
+        'jaopca:thermal_expansion.material_to_dust.obsidian',
+        'mekanism:hdpe_sheet'
+    ];
+
+    const outputRemovals = [
+
+    ];
+
+    idRemovals.forEach((id) => {
+        event.remove({ id: id });
+    });
+
+    outputRemovals.forEach((output) => {
+        event.remove({ output: output });
+    });
+});

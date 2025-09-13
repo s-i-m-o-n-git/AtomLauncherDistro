3bd41b02983d221cb06d4214a29ba25e2244e3c9
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:42 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/pneumaticcraft/assembly.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/pack/recipetypes/pneumaticcraft/assembly.js
@@ -0,0 +1,30 @@
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/pneumaticcraft/assembly/';
+
+    const recipes = [
+        {
+            input: { item: 'chroma:unassembled_pity_machine_frame', count: 1 },
+            output: { item: 'industrialforegoing:machine_frame_pity', count: 1 },
+            program: 'drill',
+            id: 'industrialforegoing:machine_frame_pity'
+        },
+        {
+            input: { item: 'minecraft:obsidian', count: 1 },
+            output: { item: 'mekanism:dust_obsidian', count: 2 },
+            program: 'drill',
+            id: 'mekanism:enriching/conversion/obsidian_to_obsidian_dust'
+        },
+    ];
+
+    recipes.forEach((recipe) => {
+        recipe.input.type = 'pneumaticcraft:stacked_item';
+        event
+            .custom({
+                type: `pneumaticcraft:assembly_${recipe.program}`,
+                input: recipe.input,
+                result: recipe.output,
+                program: recipe.program
+            })
+            .id(recipe.id);
+    });
+});

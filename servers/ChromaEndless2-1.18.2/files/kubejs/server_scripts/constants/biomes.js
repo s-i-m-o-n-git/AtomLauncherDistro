a64c021c3e272a68105a68d6d028072d68253cb0
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:13 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/constants/biomes.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/constants/biomes.js
@@ -0,0 +1,92 @@
+//priority: 1000
+
+const end_biomes = [
+    'minecraft:the_end',
+    'minecraft:the_void',
+    'minecraft:small_end_islands',
+    'minecraft:end_barrens',
+    'minecraft:end_highlands',
+    'minecraft:end_midlands'
+];
+
+const nether_biomes = [
+    'minecraft:nether_wastes',
+    'minecraft:basalt_deltas',
+    'minecraft:warped_forest',
+    'minecraft:crimson_forest',
+    'minecraft:soul_sand_valley'
+];
+
+const undergarden_biomes = [
+    'undergarden:barren_abyss',
+    'undergarden:dense_forest',
+    'undergarden:forgotten_field',
+    'undergarden:frostfields',
+    'undergarden:gronglegrowth',
+    'undergarden:mushroom_bog',
+    'undergarden:smog_spires',
+    'undergarden:smogstem_forest',
+    'undergarden:wigglewood_forest'
+];
+
+const blueskies_biomes = [
+    'blue_skies:brightlands',
+    'blue_skies:brisk_meadow',
+    'blue_skies:brumble_forest',
+    'blue_skies:calming_skies',
+    'blue_skies:crescent_orchard',
+    'blue_skies:crescent_orchard_lake',
+    'blue_skies:crystal_dunes',
+    'blue_skies:crystal_dunes_spikes',
+    'blue_skies:crystal_roughs',
+    'blue_skies:deep_peeking_ocean',
+    'blue_skies:frostbitten_forest',
+    'blue_skies:frostbitten_forest_clearing',
+    'blue_skies:midday_shore',
+    'blue_skies:moonlit_reservoir',
+    'blue_skies:peeking_ocean',
+    'blue_skies:polar_highland',
+    'blue_skies:rising_creek',
+    'blue_skies:searing_grassland',
+    'blue_skies:shaded_woodlands',
+    'blue_skies:slushlands',
+    'blue_skies:snow_covered_pines',
+    'blue_skies:sunset_maple_forest',
+    'blue_skies:unorthodox_valley'
+];
+
+const glacio_biomes = [
+    'beyond_earth:glacio',
+    'beyond_earth:glacio_ice_spikes'
+];
+
+const venus_biomes = [
+    'beyond_earth:infernal_venus_barrens',
+    'beyond_earth:venus_desert'
+];
+
+const mars_biomes = [
+    'beyond_earth:mars_desert',
+    'beyond_earth:mars_ice_spikes',
+    'beyond_earth:mars_rocky_plains'
+];
+
+const mercury_biomes = [
+    'beyond_earth:mercury'
+];
+
+const moon_biomes = [
+    'beyond_earth:moon_desert'
+];
+
+const orbit_biomes = [
+    'beyond_earth:orbit'
+];
+
+const mythic_biomes = [
+    'mythicbotany:alfheim_hills',
+    'mythicbotany:alfheim_lakes',
+    'mythicbotany:alfheim_plains',
+    'mythicbotany:dreamwood_forest',
+    'mythicbotany:golden_fields'
+];

d01c0d058e69f37f491819d362db5fb28ac3d888
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-09-13 19:23:13 +0200

    Add servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/constants/cobbleworks.js

---
+++ servers/ChromaEndless2-1.18.2/files/kubejs/server_scripts/constants/cobbleworks.js
@@ -0,0 +1,26 @@
+//priority: 1000
+
+const generatableCobblestone = [
+  'minecraft:cobblestone',
+  'minecraft:cobbled_deepslate',
+  'undergarden:depthrock',
+  'undergarden:shiverstone',
+  'undergarden:tremblecrust',
+];
+
+const generatableStone = [
+  'minecraft:andesite',
+  'minecraft:diorite',
+  'minecraft:granite',
+  'minecraft:calcite',
+  'minecraft:red_sandstone',
+  'minecraft:sandstone',
+  'create:crimsite',
+  'create:limestone',
+  'create:ochrum',
+  'create:scorchia',
+  'create:scoria',
+  'create:asurine',
+  'create:veridium'
+];
+const generatableBasalt = [];
